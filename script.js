275ac88560ef14a6ba3fdfa8cb8e655c48670fff
itsmechichay <132367334+itsmechichay@users.noreply.github.com>
2024-02-19 21:21:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+    function myMenuFunction(){
+      var menuBtn = document.getElementById("myNavMenu");
+
+      if(menuBtn.className === "nav-menu"){
+        menuBtn.className += " responsive";
+      } else {
+        menuBtn.className = "nav-menu";
+      }
+    }
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+    window.onscroll = function() {headerShadow()};
+
+    function headerShadow() {
+      const navHeader =document.getElementById("header");
+
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+
+        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+
+      } else {
+
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+
+      }
+    }
+
+
+/* ----- TYPING EFFECT ----- */
+   var typingEffect = new Typed(".typedText",{
+      strings : ["Niña Jorene,"],
+      loop : true,
+      typeSpeed : 100, 
+      backSpeed : 80,
+      backDelay : 10000
+   })
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+   const sr = ScrollReveal({
+          origin: 'top',
+          distance: '80px',
+          duration: 2000,
+          reset: true     
+   })
+
+  /* -- HOME -- */
+  sr.reveal('.featured-text-card',{})
+  sr.reveal('.featured-name',{delay: 100})
+  sr.reveal('.featured-text-info',{delay: 200})
+  sr.reveal('.featured-text-btn',{delay: 200})
+  sr.reveal('.social_icons',{delay: 200})
+  sr.reveal('.featured-image',{delay: 300})
+  sr.reveal('.featured-image',{delay: 300})
+
+
+  const sr1 = ScrollReveal({
+    origin: 'bottom',
+    distance: '80px',
+    duration: 2000,
+    reset: true     
+})
+
+  sr1.reveal('.contact-button',{delay: 100})
+  
+
+  /* -- PROJECT BOX -- */
+  sr.reveal('.project-box',{interval: 200})
+
+  /* -- HEADINGS -- */
+  sr.reveal('.top-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+  /* -- ABOUT INFO & CONTACT INFO -- */
+  const srLeft = ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+  })
+  
+  srLeft.reveal('.about-info',{delay: 100})
+  srLeft.reveal('.cert-info',{delay: 100})
+  srLeft.reveal('h3',{delay: 100})
+  srLeft.reveal('.contact-info',{delay: 100})
+
+  /* -- ABOUT SKILLS & FORM BOX -- */
+  const srRight = ScrollReveal({
+    origin: 'right',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+  })
+  
+  srRight.reveal('.skills-box',{delay: 100})
+  srRight.reveal('.form-control',{delay: 100})
+  srRight.reveal('.lead-box',{delay: 50})
+  srRight.reveal('.cell-content',{delay: 100})
+
+
+/* ----- CHANGE ACTIVE LINK ----- */
+  
+  const sections = document.querySelectorAll('section[id]')
+
+  function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach(current =>{
+      const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute('id')
+
+      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+
+          document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+      }  else {
+
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+      }
+    })
+  }
+
+  window.addEventListener('scroll', scrollActive)
+
+
+  // copy to clipboard
+
+  function CopyMe(TextToCopy) {
+    var TempText = document.createElement("input");
+    TempText.value = TextToCopy;
+    document.body.appendChild(TempText);
+    TempText.select();
+    
+    document.execCommand("copy");
+    document.body.removeChild(TempText);
+    
+    Swal.fire({
+        title: 'Text Copied!',
+        text: 'Copied the text: ' + TempText.value,
+        icon: 'success',
+        timer: 2000, // Auto close after 2 seconds
+        showConfirmButton: false
+    });
+  }
+
